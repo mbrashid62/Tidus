9e1060b1ebfca35cd40cdcdd3cd4b70d22f81bd3
Bill Rashid <mohammad-rashid@uiowa.edu>
2017-01-22 17:21:13 -0600

    styling improvements for navbar

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -5,15 +5,19 @@ import Button from 'react-button';
 
 const Header = ({loading, isSignedIn, user, signOutFunc}) => {
   return (
-    <nav>
-      <IndexLink to="/" activeClassName="active">|  Home  |</IndexLink>
-        {!isSignedIn && <Link to="/login" activeClassName="active">  Login  |</Link>}
-        {!isSignedIn && <Link to="/register" activeClassName="active">  Register  |</Link>}
-        <Link to="/courses" activeClassName="active">  Courses  |</Link>
-        <Link to="/about" activeClassName="active">  About  |</Link>
-        {isSignedIn && <Button onClick={signOutFunc}>  Sign Out  |</Button>}
-        {isSignedIn && <p>Hello, {user.email}!</p>}
-        {loading && <LoadingDots interval={100} dots={20}/>}
+    <nav className="navbar navbar-default">
+        <div className="container-fluid">
+            <ul className="nav navbar-nav navbar-right">
+                <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
+                <li>{!isSignedIn && <Link to="/login" activeClassName="active">Login</Link>}</li>
+                <li>{!isSignedIn && <Link to="/register" activeClassName="active">Register</Link>}</li>
+                <li><Link to="/courses" activeClassName="active">Courses</Link></li>
+                <li><Link to="/about" activeClassName="active">About</Link></li>
+                {isSignedIn && <li><p className="navbar-text">You are logged in as {user.email}!</p></li>}
+                {isSignedIn && <li><Button className="btn btn-default navbar-btn" onClick={signOutFunc}>Logout</Button></li>}
+                <li></li>
+            </ul>
+        </div>
     </nav>
   );
 };
