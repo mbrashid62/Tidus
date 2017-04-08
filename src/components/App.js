5bef8a4e198726350d041519d2cf0d96b6179877
Bill Rashid <mohammad-rashid@uiowa.edu>
2017-04-08 16:10:42 -0500

    button styling update

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@
 import React, {PropTypes} from 'react';
 import Header from './common/Header';
 import {connect} from 'react-redux';
+import Button from 'react-button';
 
 class App extends React.Component {
 
@@ -20,13 +21,11 @@ class App extends React.Component {
               loading={this.props.loading}
             />
             {this.props.children}
-
             <div className="tidus-footer">
                 <p>Tidus is built with React, Redux, ES6, and other cool technologies.</p>
                 <a href="http://tidus-music.herokuapp.com/">http://tidus-music.herokuapp.com/</a>
                 <p>This app is developed by <a href="https://github.com/mbrashid62">mbrashid62</a>.</p>
             </div>
-
           </div>
       );
     }
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router';
+import Button from 'react-button';
 
 class HomePage extends React.Component {
   render() {
@@ -12,6 +13,7 @@ class HomePage extends React.Component {
                 determine what tracks are appropriate for the gym, for a party, for a lazy sunday at home, and more.</p>
                 <p>This app lets you peak underneath the hood and see some audio data Spotify has for your playlists.</p>
                 <p>Please login and then visit the Dashboard to try it out.</p>
+                <Button className="btn btn-lg action-btn" onClick={()=> {}} theme={{overStyle:{background:'black'}}}>Connect to Spotify</Button>
             </div>
         </div>
     );
