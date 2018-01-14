c4b390a5131540f65126098f01c1f6d8c69337c9
Bill Rashid <bill.rashid12@gmail.com>
2018-01-13 18:59:05 -0600

    release loader fix

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -30,8 +30,7 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
-      {test: /(\.css)$/, loader: ExtractTextPlugin.extract("css?sourceMap")},
-      {test: /\.scss$/, loaders: ['style', 'sass']},
+      {test: /\.(scss|css)$/, loaders: ['style', 'css', 'sass']},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
       {test: /\.(woff|woff2)$/, loader: "url?prefix=font/&limit=5000"},
       {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
